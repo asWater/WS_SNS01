b068ce787b5d185b87c54e9d6561f35a4a7d5285
As Water <radio801@gmail.com>
2015-02-21 22:56:56 +0900

    several fixes.

--- WS_SNS01/header.php
+++ WS_SNS01/header.php
@@ -22,31 +22,31 @@ else
 
 echo <<<_END
 	<title>$appName$userStr</title>
-	<link rel='stylesheet' href='styles.css' type='text/css' />
+	<link rel='stylesheet' href='http://localhost/prj/WS_SNS01/styles.css' type='text/css' />
 	</head>
 	<body>
 	<div class='appname'>$appName$userStr</div>
-	<script src='OSC.js'></script>
-	<script src='checkSignup.js'></script>
+	<script src='http://localhost/prj/WS_SNS01/OSC.js'></script>
+	<script src='http://localhost/prj/WS_SNS01/checkSignup.js'></script>
 _END;
 
 if ($loggedIn)
 {
 	echo "<br><ul class='menu'>" .
-		 "<li><a href='index.php?view=$user'>Home</a></li>" .
-		 "<li><a href='members.php'>Members</a></li>" .
-		 "<li><a href='friends.php'>Friends</a></li>" .
-		 "<li><a href='members.php?view=$user'>Profile</a></li>" .
-		 "<li><a href='profile.php'>Edit Profile</a></li>" .
-		 "<li><a href='logout.php'>Log out</a></li>" .
+		 "<li><a href='http://localhost/prj/WS_SNS01/index.php?view=$user'>Home</a></li>" .
+		 "<li><a href='http://localhost/prj/WS_SNS01/members.php'>Members</a></li>" .
+		 "<li><a href='http://localhost/prj/WS_SNS01/friends.php'>Friends</a></li>" .
+		 "<li><a href='http://localhost/prj/WS_SNS01/members.php?view=$user'>Profile</a></li>" .
+		 "<li><a href='http://localhost/prj/WS_SNS01/profile.php'>Edit Profile</a></li>" .
+		 "<li><a href='http://localhost/prj/WS_SNS01/logout.php'>Log out</a></li>" .
 		 "</ul><br>";
 }
 else
 {
 	echo ("<br /><ul class='menu'>" .
-		  "<li><a href='index.php'>Home</a></li>" .
-		  "<li><a href='signup.php'>Sign up</a></li>" .
-		  "<li><a href='login.php'>Log in</a></li>" .
+		  "<li><a href='http://localhost/prj/WS_SNS01/index.php'>Home</a></li>" .
+		  "<li><a href='http://localhost/prj/WS_SNS01/signup.php'>Sign up</a></li>" .
+		  "<li><a href='http://localhost/prj/WS_SNS01/login.php'>Log in</a></li>" .
 		  "</ul><br>");
 		  //"<span class='info'>&#8658; You must be logged in to View this page.</span><br><br>");
 }
